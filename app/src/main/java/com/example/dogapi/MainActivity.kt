4ce56848b2a2814cbc28fc86dd9f827e30708cd2
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-05-17 15:22:20 -0300

    add more files

--- app/src/main/java/com/example/dogapi/MainActivity.kt
+++ app/src/main/java/com/example/dogapi/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.example.dogapi.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        binding.buttonPesquisar.setOnClickListener { getImage() }
+    }
+
+    private fun getImage() {
+        val url = "https://dog.ceo/"
+        val retrofitClient = retrofitInstance(url)
+        val endpoint = retrofitClient.create()
+
+//        val urlImage = "https://media.istockphoto.com/photos/canadian-flag-with-nice-satin-texture-picture-id171263903?k=20&m=171263903&s=170667a&w=0&h=ounPYNo_TU_Vg5dLT39PzcX0uxf2BB0DvoXZF4P_dPI="
+//        Picasso.get().load(urlImage).into(binding.imageDog)
+    }
+
+    private fun retrofitInstance(url: String): Retrofit {
+        return Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build()
     }
 }
---
+++ app/src/main/java/com/example/dogapi/api/Endpoint.kt
@@ -0,0 +1,4 @@
+package com.example.dogapi.api
+
+class Endpoint {
+}
